f0097235ff3d9c049bf53e4ce1596d98abeef0f8
lanasergeeva <marikakopina@gmail.com>
2021-09-27 17:46:01 +0300

    3. posts.jsp - список вакансий. [#276521 #209115]

--- src/main/java/dream/store/Store.java
+++ src/main/java/dream/store/Store.java
@@ -1,5 +1,6 @@
 package dream.store;
 
+import dream.model.Candidate;
 import dream.model.Post;
 
 import java.util.Collection;
@@ -7,17 +8,19 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-
 public class Store {
 
     private static final Store INST = new Store();
-
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
@@ -26,5 +29,9 @@ public class Store {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
