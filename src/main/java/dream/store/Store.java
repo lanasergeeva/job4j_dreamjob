ecf43d012c00fdcd81ad345014f2320c4302dcd9
lanasergeeva <marikakopina@gmail.com>
2021-09-28 12:56:03 +0300

    4. candicates.jsp - список кандидатов. [#276522 #209120]

--- src/main/java/dream/store/Store.java
+++ src/main/java/dream/store/Store.java
@@ -8,13 +8,12 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 public class Store {
 
     private static AtomicInteger postid = new AtomicInteger(4);
     private static final Store INST = new Store();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
