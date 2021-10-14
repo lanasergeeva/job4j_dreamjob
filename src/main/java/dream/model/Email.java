442a97ef0b5790f74b29a2de00a7c09adb1f2510
lanasergeeva <marikakopina@gmail.com>
2021-10-14 13:07:16 +0300

    JSON формат. [#238554]

---
+++ src/main/java/dream/model/Email.java
@@ -0,0 +1,13 @@
+package dream.model;
+
+public class Email {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/dream/servlet/GreetingServlet.java
+++ src/main/java/dream/servlet/GreetingServlet.java
@@ -1,19 +1,45 @@
 package dream.servlet;
 
+import dream.model.Email;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 public class GreetingServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
-    }
+
+        private final List<Email> emails = new CopyOnWriteArrayList<>();
+
+        private static final Gson GSON = new GsonBuilder().create();
+
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            Email email = GSON.fromJson(req.getReader(), Email.class);
+            emails.add(email);
+
+            resp.setContentType("application/json; charset=utf-8");
+            OutputStream output = resp.getOutputStream();
+            String json = GSON.toJson(email);
+            output.write(json.getBytes(StandardCharsets.UTF_8));
+            output.flush();
+            output.close();
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            resp.setContentType("application/json; charset=utf-8");
+            OutputStream output = resp.getOutputStream();
+            String json = GSON.toJson(emails);
+            output.write(json.getBytes(StandardCharsets.UTF_8));
+            output.flush();
+            output.close();
+        }
 }
